605d567ddddc9cf4a75e7a0edfc6146724195f77
wangyafeng <296672810@qq.com>
2020-12-02 18:02:10 +0800

    add stream() to return fstream object

--- log_manger.cpp
+++ log_manger.cpp
@@ -2,25 +2,26 @@
 #include <iostream>
 #include <sstream>
 
-#ifdef __WIN32
-#else
+#ifdef __linux
 #include <sys/stat.h>
 #include <sys/time.h>
+#elif __WIN32
+
 #endif
 
 #include "log_manger.h"
 
 namespace bm {
 
-vector<string> split(const string& str, const string& sp)
+std::vector<std::string> split(const std::string& str, const std::string& sp)
 {
-    vector<string> result;
-    string::size_type start = 0;
-    string::size_type pos = 0;
+    std::vector<std::string> result;
+    std::string::size_type start = 0;
+    std::string::size_type pos = 0;
     do
     {
         pos = str.find(sp, start);
-        if(pos != string::npos)
+        if(pos != std::string::npos)
         {
             result.push_back(str.substr(start, pos - start));
             start = pos + sp.size();
@@ -28,15 +29,15 @@ vector<string> split(const string& str, const string& sp)
         else
             result.push_back(str.substr(start));
     }
-    while(pos != string::npos);
+    while(pos != std::string::npos);
 
     return result;
 }
-void mkdirs(const string& path, mode_t mode)
+void mkdirs(const std::string& path, mode_t mode)
 {
-    vector<string> paths = split(path, "/");
+    std::vector<std::string> paths = split(path, "/");
 
-    string dir;
+    std::string dir;
     for(auto i : paths)
     {
         dir += i + "/";
@@ -44,7 +45,7 @@ void mkdirs(const string& path, mode_t mode)
     }
 }
 
-log_manger::log_manger(const string& log_path):
+log_manger::log_manger(const std::string& log_path):
     m_log_path(log_path)
 {
     mkdirs(log_path, 0766);
@@ -58,7 +59,7 @@ log_manger::~log_manger()
     m_fs_info.close();
 }
 
-void log_manger::write(const char* curr_file_name, const char* curr_func_name, int line_num, log_leval leval, const string& content)
+void log_manger::write(const char* curr_file_name, const char* curr_func_name, int line_num, log_leval leval, const std::string& content)
 {
     _write(curr_file_name, curr_func_name, line_num);
     _write(leval);
@@ -68,44 +69,43 @@ void log_manger::write(const char* curr_file_name, const char* curr_func_name, i
 }
 void log_manger::_endl()
 {
-    lock_guard<mutex> lock(m_lock);
+    lock_guard_mutex lock(m_lock);
     m_fs_info << std::endl;
 }
 void log_manger::_flush()
 {
-    lock_guard<mutex> lock(m_lock);
+    lock_guard_mutex lock(m_lock);
     m_fs_info.flush();
 }
 void log_manger::_write(log_leval leval)
 {
-    lock_guard<mutex> lock(m_lock);
+    lock_guard_mutex lock(m_lock);
     m_fs_info << get_log_leval_string(leval) << ":";
 }
-void log_manger::_write(const string& content)
+void log_manger::_write(const std::string& content)
 {
-    lock_guard<mutex> lock(m_lock);
+    lock_guard_mutex lock(m_lock);
     m_fs_info << content;
 }
 void log_manger::_write(const char* curr_file_name, const char* curr_func_name, int line_num)
 {
-    lock_guard<mutex> lock(m_lock);
+    lock_guard_mutex lock(m_lock);
     m_fs_info << curr_file_name << "##" << curr_func_name << "##" << line_num;
     m_fs_info << get_current_time();
 }
 
 void log_manger::_open_file()
 {
-    lock_guard<mutex> lock(m_lock);
-    string t = get_current_date();
-    string file_path = m_log_path + "/info_" + t + ".log";
-    ifstream f(file_path);
-    if(f.good())
+    lock_guard_mutex lock(m_lock);
+    std::string t = get_current_date();
+    std::string file_path = m_log_path + "/info_" + t + ".log";
+    if(m_fs_info.is_open())
         return;
     
-    m_fs_info.open(file_path, ios::app | ios::in | ios::out);
+    m_fs_info.open(file_path, std::ios::app | std::ios::in | std::ios::out);
 }
 
-string log_manger::get_current_date()
+std::string log_manger::get_current_date()
 {
     char d[50] = {0};
 
@@ -115,9 +115,9 @@ string log_manger::get_current_date()
 
     sprintf(d, "%d-%d-%d", local->tm_year + 1900, local->tm_mon + 1, local->tm_mday);
 
-    return string(d);
+    return std::string(d);
 }
-string log_manger::get_current_time()
+std::string log_manger::get_current_time()
 {
     struct tm *local = NULL;
     time_t t = time(NULL);
@@ -128,12 +128,12 @@ string log_manger::get_current_time()
            local->tm_year + 1900, local->tm_mon + 1, local->tm_mday,
            local->tm_hour, local->tm_min, local->tm_sec);
 
-    return string(d);
+    return std::string(d);
 }
 
-string log_manger::get_log_leval_string(log_leval leval)
+std::string log_manger::get_log_leval_string(log_leval leval)
 {
-    string result;
+    std::string result;
     switch(leval)
     {
     case INFO:
@@ -150,14 +150,6 @@ string log_manger::get_log_leval_string(log_leval leval)
         break;
     }
     return result;
-}
-
-log_manger& log_manger::operator<<(string content)
-{
-    lock_guard<mutex> lock(m_lock);
-    m_fs_info << content;
-    // m_fs_info << endl;
-    return *this;
 }
 
 }
--- log_manger.h
+++ log_manger.h
@@ -8,7 +8,7 @@
 #include <mutex>
 #include <condition_variable>
 #include <fstream>
-using namespace std;
+
 namespace bm{
 enum log_leval
 {
@@ -17,48 +17,45 @@ enum log_leval
     ERROR,
 };
 
-vector<string> split(const string& str, const string& sp);
-void mkdirs(const string& path, mode_t mode);
+std::vector<std::string> split(const std::string& str, const std::string& sp);
+void mkdirs(const std::string& path, mode_t mode);
+
+typedef std::lock_guard<std::mutex> lock_guard_mutex;
 
 class log_manger;
 
 class log_manger
 {
 public:
-    log_manger(const string& log_path = "");
+    log_manger(const std::string& log_path = "");
     ~log_manger();
 
-    void write(const char* curr_file_name, const char* curr_func_name, int line_num, log_leval leval, const string& content);
+    void write(const char* curr_file_name, const char* curr_func_name, int line_num, log_leval leval, const std::string& content);
 
-    log_manger& operator<<(string content);
-    template<class T>
-    log_manger& operator<<(T content)
+    std::fstream& stream()
     {
-        lock_guard<mutex> lock(m_lock);
-        m_fs_info << content;
-        return *this;
+        return m_fs_info;
     }
 
     void _write(const char* curr_file_name, const char* curr_func_name, int line_num);
     void _write(log_leval leval);
-    void _write(const string& content);
+    void _write(const std::string& content);
     void _endl();
     void _flush();
     void _open_file();
 private:
 
-    string get_current_date();
-    string get_current_time();
+    std::string get_current_date();
+    std::string get_current_time();
 
-    string get_log_leval_string(log_leval leval);
+    std::string get_log_leval_string(log_leval leval);
 
 private:
-    string m_log_path;
-    fstream m_fs_info;
-//    fstream m_fs_error;
-    mutex m_lock;
-};
+    std::string m_log_path;
+    std::fstream m_fs_info;
 
+    std::mutex m_lock;
+};
 
 
 static log_manger s_log_manger("log/today");
@@ -73,6 +70,6 @@ inline void bm_write_log(const char* curr_file_name, const char* curr_func_name,
     s_log_manger._write(leval);
 }
 #define LOG(info) bm_write_log(__FILE__, __FUNCTION__, __LINE__, info);\
-    s_log_manger
+    s_log_manger.stream()
 
 #endif
